c3b03c62d582eeb574d8256629f68d17f87fef0f
Trotteriniku <stefanozidda3@gmail.com>
2023-11-08 18:50:58 +0100

    milestone 1

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle'
+    }
+</script>
+
+<style lang="css" scoped>
+        h1{
+            margin-top: 50px;
+            text-align: center;
+            text-transform: uppercase;
+            color: orange;
+        }
+</style>
